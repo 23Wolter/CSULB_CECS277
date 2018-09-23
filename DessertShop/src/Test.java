58c73b140b1cba713e5b309ebebbf9bb0619f796
jopl93 <josephine@vestnet.dk>
2018-09-22 23:09:52 -0700

    Testing done for candy class

--- DessertShop/src/Test.java
+++ DessertShop/src/Test.java
@@ -11,40 +11,39 @@ public class Test {
     public static void main (String[] args){
 
         //Testing the Candy class
+        System.out.println("***CANDY CLASS TESTING***");
 
         //Default constructor test
+        System.out.println("***Default constructor testing***");
         Candy defaultCandy = new Candy();
         System.out.println(defaultCandy);
+        System.out.println("");
 
+        //Method testing for defaultCandy item
+        System.out.println("**Methods testing***");
         System.out.println("Name: " + defaultCandy.getName());
         System.out.println("Cost: " + defaultCandy.getCost());
         System.out.println("Price pr lbs: " + defaultCandy.getPricePrLbs());
         System.out.println("Weight: " + defaultCandy.getWeight());
         System.out.println("Calories: " + defaultCandy.getCalories());
+        System.out.println("");
 
+        //Multi-argument constructor test
+        System.out.println("**Multi-argument constructor testing***");
+        Candy candy = new Candy("gummyBear", 3.54, 75, 200);
+        System.out.println(candy);
+        System.out.println("");
+
+        //Method testing for candy item
+        System.out.println("**Methods testing ***");
+        System.out.println("Name: " + candy.getName());
+        System.out.println("Cost: " + candy.getCost());
+        System.out.println("Price pr lbs: " + candy.getPricePrLbs());
+        System.out.println("Weight: " + candy.getWeight());
+        System.out.println("Calories: " + candy.getCalories());
         System.out.println("");
 
 
-        //Multi-argument constructor test
-        Candy candy = new Candy("gummy", 3.54, 7, 200);
-
-        //Testing method getCost()
-        int price = candy.getCost();
-        System.out.println("this is the price of candy " + price);
-
-        System.out.println("Name:" + candy.getName());
-        System.out.println("Weight: " + candy.getWeight());
-        System.out.println("Price pr LBS: " + candy.getPricePrLbs());
-
-        //Testing IceCream class
-        IceCream iceCream = new IceCream(400);
-        int iceCreamPrice = iceCream.getCost();
-        System.out.println("This is the price of the icecream: " + iceCreamPrice);
-
-        //Testing SundaeClass
-        Sundae sundae = new Sundae(100, "sundae", 500);
-        int sundaePrice = sundae.getCost();
-        System.out.println("This is the price of the sundae: " + sundaePrice);
 
     }
 

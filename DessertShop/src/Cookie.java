a68f33fe8569775f916358faa301b09c7cfd7b1b
23Wolter <wolter.nielsen@gmail.com>
2018-09-22 23:16:03 -0700

    java docs + cookie
    created Java Docs for Ice Cream Class

--- DessertShop/src/Cookie.java
+++ DessertShop/src/Cookie.java
@@ -1,10 +1,76 @@
 /*
     Josephine & Oliver
     September 12, 2018
-    Purpose:
+    Purpose: a cookie item has a number, a price, and a price pr. dozen
     Inputs:
     Output:
 */
 
-public class Cookie {
+//this class represents a cookie with a number, price, and price pr. dozen
+/**
+ * this class represents a cookie with a number, price, and price pr. dozen
+ * @author Josephine & Oliver
+ * @date 09/22/18
+ */
+public class Cookie extends DessertItem {
+
+    private int number; //the number of cookies in a portion
+    private double pricePrDz; //the price pr. dozen cookies
+    private double calories; //the calories in the number of cookies
+
+    //default constructor, setting the number of cookies, the calories, and the price pr. dz.
+    /**
+     * default constructor, setting the number of cookies, the calories, and the price pr. dz.
+     */
+    public Cookie() {
+        super.name = "Chocolate Chip";
+        number = 5;
+        pricePrDz = 279;
+        calories = 355;
+    }
+
+    //multi-argument constructor sets the name, number, price pr. dz., and calories of the cookie item
+    /**
+     * multi-argument constructor sets the name, number, price pr. dz., and calories of the cookie item
+     * @param name - the name of the cookie
+     * @param number - the number of cookies in a portion
+     * @param pricePrDz - the price per dozen cookies
+     * @param calories - the calories in this number of cookies
+     */
+    public Cookie(String name, int number, double pricePrDz, double calories) {
+        super.name = name;
+        this.number = number;
+        this.pricePrDz = pricePrDz;
+        this.calories = calories;
+    }
+
+    //returns the number of cookies in this portion
+    /**
+     * @return the number of cookies in this portion
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    //returns the price per dozen cookies
+    /**
+     * @return the price per dozen cookies
+     */
+    public double getPricePrDz() {
+        return pricePrDz;
+    }
+
+    //returns the calories of a portion of cookies
+    /**
+     * @return the calories of a portion of cookies
+     */
+    public double getCalories() {
+        return calories;
+    }
+
+    @Override
+    public int getCost() {
+
+        return 0;
+    }
 }
--- DessertShop/src/IceCream.java
+++ DessertShop/src/IceCream.java
@@ -1,30 +1,61 @@
 /*
     Josephine & Oliver
     September 12, 2018
-    Purpose:
-    Inputs:
-    Output:
+    Purpose: an ice cream has just a cost, and can be a sundae if topping is added
+    Inputs: the cost of the ice cream and the amount of calories
+    Output: the cost of the ice cream and the calories
 */
 
+//this class represents an ice cream item
+/**
+ * //this class represents an ice cream item
+ * @author Josephine & Oliver
+ * @date 09/22/18
+ */
 public class IceCream extends DessertItem {
 
+    //the cost of an ice cream, the access modifier is protected because the Sundae class needs access to this variabel
     protected int cost;
+    protected double calories; //the calories of an ice cream, is protected so Sundae can access it
 
-    //Default constructor
+    //default constructor, sets cost to 500 cents, and the calories to 470
+    /**
+     * default constructor, sets cost to 500 cents, and the calories to 470
+     */
     public IceCream(){
         cost = 500;
+        calories = 470;
     }
 
-    //Constructor takes a cost
-    //Input: a cost
-    public IceCream(int cost){
+    //constructor takes a cost in cents
+    //input: a cost
+    /**
+     * multi-argument constructor takes a cost in cents, and an amount of calories
+     * @param cost - the cost of an ice cream in cents, and an amount of calories
+     * @param calories - the calories of an ice cream
+     */
+    public IceCream(int cost, double calories){
         this.cost = cost;
+        this.calories = calories;
     }
 
     //getCost overwritten from DessertItem Class
-    //Output: cost
+    //output: cost
+    /**
+     * getCost overwritten from DessertItem Class
+     * @return the cost of the ice cream item, which is also a dessert item
+     */
     @Override
     public int getCost() {
         return cost;
+    }
+
+    //returns the calories of the ice cream class
+    //output: calories
+    /**
+     * @return the amount of calories of an ice cream
+     */
+    public double getCalories() {
+        return calories;
     }
 }

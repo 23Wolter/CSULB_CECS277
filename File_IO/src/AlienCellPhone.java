fa5fd995c36318b0ddd5d08446acd5921d7f5d63
jopl93 <josephine@vestnet.dk>
2018-10-10 22:26:16 -0700

    Java docs header added.

--- File_IO/src/AlienCellPhone.java
+++ File_IO/src/AlienCellPhone.java
@@ -1,3 +1,12 @@
+/**
+ * Josephine & Oliver
+ * October 9, 2018
+ * Purpose: This program simulates a vending machine
+ * Inputs: numberOfProducts, index, product, coin
+ * Output: A list of products, a specific product, number of coins, value of coins, value of coins in repo
+ * @author Josephine Plass-Nielsen & Oliver Nielsen
+ * @version 1.0
+ */
 public interface AlienCellPhone {
     public void alienSendText(String fileName);
     public void alienReadText(String fileName);
--- File_IO/src/EarthToAlienAdapter.java
+++ File_IO/src/EarthToAlienAdapter.java
@@ -1,5 +1,11 @@
 /**
- * Created by josep on 10-10-2018.
+ * Josephine & Oliver
+ * October 10, 2018
+ * Purpose: This program simulates a vending machine
+ * Inputs: numberOfProducts, index, product, coin
+ * Output: A list of products, a specific product, number of coins, value of coins, value of coins in repo
+ * @author Josephine Plass-Nielsen & Oliver Nielsen
+ * @version 1.0
  */
 public class EarthToAlienAdapter {
 }

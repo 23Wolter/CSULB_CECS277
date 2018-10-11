65701a13abf8d93a0dc6d2696c27525348b103bb
jopl93 <josephine@vestnet.dk>
2018-10-10 22:31:35 -0700

    Java docs header changed.

--- File_IO/src/EarthToAlienAdapter.java
+++ File_IO/src/EarthToAlienAdapter.java
@@ -1,11 +1,12 @@
 /**
  * Josephine & Oliver
  * October 10, 2018
- * Purpose: This program simulates a vending machine
- * Inputs: numberOfProducts, index, product, coin
- * Output: A list of products, a specific product, number of coins, value of coins, value of coins in repo
+ * Purpose:
+ * Inputs:
+ * Output:
  * @author Josephine Plass-Nielsen & Oliver Nielsen
  * @version 1.0
  */
+
 public class EarthToAlienAdapter {
 }
--- File_IO/src/Main.java
+++ File_IO/src/Main.java
@@ -1,12 +1,13 @@
 /**
  * Josephine & Oliver
  * October 9, 2018
- * Purpose: This program simulates a vending machine
- * Inputs: numberOfProducts, index, product, coin
- * Output: A list of products, a specific product, number of coins, value of coins, value of coins in repo
+ * Purpose:
+ * Inputs:
+ * Output:
  * @author Josephine Plass-Nielsen & Oliver Nielsen
  * @version 1.0
  */
+
 public class Main {
     public static void main(String[] args){
 

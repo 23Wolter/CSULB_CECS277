128ef5cae68e73ecf985e3686dcf38f37c230112
jopl93 <josephine@vestnet.dk>
2018-10-30 13:36:42 -0700

    Added hm.put()

--- Collections_Project/src/HashMapTest.java
+++ Collections_Project/src/HashMapTest.java
@@ -8,11 +8,26 @@ public class HashMapTest {
 
     public static void main(String[] args){
 
+        //Creating HashMap
         Map<String, Integer> hm = new HashMap<>();
 
+       //Initializes the chosen file
         File inputFile = new File("QWords_points.txt");
+
+        //Reads from file
         try {
             Scanner in = new Scanner(inputFile);
+            while(in.hasNext()){
+
+                String line = in.nextLine();
+                String[] wordsAndPoints = line.split(" ");
+
+                String currentWord = wordsAndPoints[0];
+                Integer currentPoint = Integer.parseInt(wordsAndPoints[0]);
+
+                hm.put(currentWord,currentPoint);
+
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }

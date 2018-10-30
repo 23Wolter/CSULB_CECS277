33d3d0267acb57bd345f348812e024873f2cf838
23Wolter <wolter.nielsen@gmail.com>
2018-10-30 13:17:56 -0700

    WIP iterator pattern

--- Collections_Project/src/HashMap_Test.java
+++ Collections_Project/src/HashMap_Test.java
@@ -2,11 +2,11 @@ public class HashMap_Test {
 
     public static void main(String[] args){
 
-        Map<String, int> hm = new Haspmap<>();
+        /*Map<String, int> hm = new Haspmap<>();
 
         File inputFile = new File("QWords_points.txt");
         Scanner in = new Scanner();
-
+*/
 
         System.out.println();
 
--- Collections_Project/src/TreeMapTest.java
+++ Collections_Project/src/TreeMapTest.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * Josephine and Oliver
@@ -10,24 +12,37 @@ import java.util.Scanner;
  * Output: the time it takes to load the file
  */
 
-public class TreeMap_Test {
+public class TreeMapTest implements MainTest {
+
+    @Override
+    public Iterator createIterator() {
+        return null;
+    }
 
     public static void main(String[] args) {
 
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+
         try {
 
-            File file = new File('QWords_points.txt');
+            File file = new File("QWords_points.txt");
             Scanner inputFile = new Scanner(file);
 
             while (inputFile.hasNext()) {
 
                 String line = inputFile.nextLine();
+                String[] wordsAndPoints = line.split(" ");
+                String word = wordsAndPoints[0];
+                int point = Integer.parseInt(wordsAndPoints[1]);
 
+                treeMap.put(word, point);
             }
 
         }  catch (FileNotFoundException e) {
             System.out.println("File not found: " + e);
         }
+
+        System.out.println(treeMap);
     }
 
 }

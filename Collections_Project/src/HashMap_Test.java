debfb1190421e7f151b8ec808e3fd55db6d84916
jopl93 <josephine@vestnet.dk>
2018-10-30 12:41:15 -0700

    .

--- Collections_Project/src/HashMap_Test.java
+++ Collections_Project/src/HashMap_Test.java
@@ -1,2 +1,14 @@
 public class HashMap_Test {
+
+    public static void main(String[] args){
+
+        Map<String, int> hm = new Haspmap<>();
+
+        File inputFile = new File("QWords_points.txt");
+        Scanner in = new Scanner();
+
+
+        System.out.println();
+
+    }
 }
